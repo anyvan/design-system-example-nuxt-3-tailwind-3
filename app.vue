bccc65be4baa4b9aa09f1e2a939eab11a0dafc67
Dan Barclay <barclaysd@me.com>
2025-06-02 22:33:35 +0100

    feat: add design system

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div class=" w-screen h-screen bg-blue-950 flex justify-center items-center gap-8 flex-col">
     <h1 class="text-gray-200 text-2xl">Nuxt 3 + Tailwind 3 playground</h1>
-    <button>Click me</button>
+    <button class="av-button">Click me</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import '@anyvan-ui/button/Button.css';
+</script>
